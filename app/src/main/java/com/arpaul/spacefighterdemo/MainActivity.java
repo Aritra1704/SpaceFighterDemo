8b4e8775625d46fc10a6ed94440627ef866bc5e7
Aritra1704 <aritra1704@gmail.com>
2017-01-15 01:34:39 +0530

    Initial Commit.

---
+++ app/src/main/java/com/arpaul/spacefighterdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.arpaul.spacefighterdemo;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //image button
+    private ImageButton buttonPlay;
+    //https://www.simplifiedcoding.net/android-game-development-tutorial-1/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setting the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //getting the button
+        buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
+
+        //adding a click listener
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //starting game activity
+        startActivity(new Intent(this, GameActivity.class));
+    }
+}
